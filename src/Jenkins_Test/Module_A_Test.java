ebe582568a152ff9b168b0dcbd53083e8de02167
akm0012 <akm0012@auburn.edu>
2014-04-07 19:41:38 -0500

    Added test that fails.

---
+++ src/Jenkins_Test/Module_A_Test.java
@@ -0,0 +1,14 @@
+package Jenkins_Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Module_A_Test {
+
+	@Test
+	public void test() {
+		assertEquals("1 + 1 = 2", 2, 2+1);
+	}
+
+}
--- src/Jenkins_Test/Module_B.java
+++ src/Jenkins_Test/Module_B.java
@@ -6,5 +6,4 @@ public class Module_B {
 		// TODO Auto-generated constructor stub
 	}
 
-
 }
