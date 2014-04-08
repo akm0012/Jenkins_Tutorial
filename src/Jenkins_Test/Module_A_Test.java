34a259bf55d403f7bcfbf24546edcc6883815ab4
akm0012 <akm0012@auburn.edu>
2014-04-08 11:37:45 -0500

    Tests now pass.

--- src/Jenkins_Test/Module_A_Test.java
+++ src/Jenkins_Test/Module_A_Test.java
@@ -11,7 +11,7 @@ public class Module_A_Test {
 
 		Module_A module_a = new Module_A();
 
-		assertEquals("This should return 1", -1, module_a.return_1());
+		assertEquals("This should return 1", 1, module_a.return_1());
 	}
 
 }
