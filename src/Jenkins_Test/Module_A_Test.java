0c8f2ea8d37982d2a7d32dd96026f96aaab2a1c4
akm0012 <akm0012@auburn.edu>
2014-04-08 17:12:28 -0500

    Added .gitignore

--- src/Jenkins_Test/Module_A_Test.java
+++ src/Jenkins_Test/Module_A_Test.java
@@ -13,5 +13,13 @@ public class Module_A_Test {
 
 		assertEquals("This should return 1", 1, module_a.return_1());
 	}
+	
+	@Test
+	public void test2() {
+
+		Module_A module_a = new Module_A();
+
+		assertEquals("This should return 1", 1, module_a.return_1());
+	}
 
 }
